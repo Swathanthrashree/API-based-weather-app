03df9e767a8c0676c0dafb9a5966c5d01413a089
Swathanthrashree <shreeswathanthra@gmail.com>
2023-12-27 13:04:14 +0530

    add

---
+++ src/api.js
@@ -0,0 +1,15 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5567c044e7msh6269b8f38f66e72p184a9fjsnee15ba782a7a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="154cce0fcbe511e2b81385736410a30b";
+
+
+
